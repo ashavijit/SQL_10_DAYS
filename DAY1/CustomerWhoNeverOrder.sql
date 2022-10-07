81207f82bd94c946f2ca46235d81f784389f7a83
ashavijit <as2440@it.jgec.ac.in>
2022-10-07 12:15:13 +0530

    sql study

---
+++ DAY1/CustomerWhoNeverOrder.sql
@@ -0,0 +1,27 @@
+/* Table: Customers
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the ID and name of a customer.
+ 
+
+Table: Orders
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| customerId  | int  |
++-------------+------+
+id is the primary key column for this table.
+customerId is a foreign key of the ID from the Customers table.
+Each row of this table indicates the ID of an order and the ID of the customer who ordered it. */
+
+
+/* # Write your MySQL query statement below */
+select C.name as Customers from Customers as C where C.id NOT IN (select O.CustomerId from Orders as O);
